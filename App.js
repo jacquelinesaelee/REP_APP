86a35823af30b73c632961d78cf0c626cd18cd3e
Jacky <jacquelinesaelee@yahoo.com>
2018-09-24 14:09:31 -0700

    Creation
    Create the ReactNative Framework for REP App

---
+++ App.js
@@ -0,0 +1,26 @@
+/*
+Main Page
+*/
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>REP</Text>
+        <Text>Raspados | Elotes | Paletas</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
